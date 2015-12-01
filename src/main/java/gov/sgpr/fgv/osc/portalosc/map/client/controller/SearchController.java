083980f507a944fc7334a8503fdbfa3f9d0e86fd
Vagner Praia da Silva <vagnerpraia@gmail.com>
2015-12-01 15:44:34 -0200

    Issue: MOSC-66 Subject: Criar a funcionalidade de configurações de conta Detail:

--- src/main/java/gov/sgpr/fgv/osc/portalosc/map/client/controller/SearchController.java
+++ src/main/java/gov/sgpr/fgv/osc/portalosc/map/client/controller/SearchController.java
@@ -28,19 +28,18 @@ public class SearchController {
 	private SearchServiceAsync searchService = GWT.create(SearchService.class);
 	private static int LIMIT = 5;
 	private Date changeDate;
-
+	
 	public void init() {
-
 		searchDiv.add(searchWidget);
+		
 		searchWidget.addFocusListener(new EventListener() {
-
 			@Override
 			public void onBrowserEvent(Event event) {
 				searchWidget.setValue("");
 			}
 		});
+		
 		searchWidget.addChangeListener(new EventListener() {
-
 			@Override
 			public void onBrowserEvent(Event event) {
 				changeDate = new Date();
@@ -56,46 +55,44 @@ public class SearchController {
 				t.schedule(DELAY);
 			}
 		});
+		
 		searchWidget.addSearchClickListener(new EventListener() {
-
 			@Override
 			public void onBrowserEvent(Event event) {
 				String criteria = searchWidget.getValue();
 				AsyncCallback<List<SearchResult>> callbackSearch = new AsyncCallback<List<SearchResult>>() {
-
+					
 					public void onFailure(Throwable caught) {
 						logger.log(Level.SEVERE, caught.getMessage());
 					}
-
+					
 					public void onSuccess(List<SearchResult> result) {
 						if (!result.isEmpty()) {
-							if (result.get(0).getType()
-									.equals(SearchResultType.STATE))
+							if (result.get(0).getType().equals(SearchResultType.STATE)){
 								History.newItem("P" + result.get(0).getId());
-							if (result.get(0).getType()
-									.equals(SearchResultType.COUNTY))
+							}
+							if (result.get(0).getType().equals(SearchResultType.COUNTY)){
 								History.newItem("P" + result.get(0).getId());
-							if (result.get(0).getType()
-									.equals(SearchResultType.OSC))
+							}
+							if (result.get(0).getType().equals(SearchResultType.OSC)){
 								History.newItem("O" + result.get(0).getId());
+							}
 						}
 					}
 				};
 				searchService.search(criteria, LIMIT, callbackSearch);
 			}
 		});
-
-
 	}
-
+	
 	private void search() {
 		String criteria = searchWidget.getValue();
 		AsyncCallback<List<SearchResult>> callbackSearch = new AsyncCallback<List<SearchResult>>() {
-
+			
 			public void onFailure(Throwable caught) {
 				logger.log(Level.SEVERE, caught.getMessage());
 			}
-
+			
 			public void onSuccess(List<SearchResult> result) {
 				searchWidget.setItems(result);
 			}
@@ -103,21 +100,20 @@ public class SearchController {
 		if (!criteria.trim().isEmpty())
 			searchService.search(criteria, LIMIT, callbackSearch);
 	}
-
+	
 	/**
 	 * @return Instância da busca
 	 */
 	public SearchController getInstance() {
 		return this;
 	}
-
+	
 	/**
 	 * @param isVisible
 	 *            indica se a busca deve estar visível ou não.
 	 */
 	protected void setVisible(boolean isVisible) {
-		Visibility visibility = isVisible ? Visibility.VISIBLE
-				: Visibility.HIDDEN;
+		Visibility visibility = isVisible ? Visibility.VISIBLE : Visibility.HIDDEN;
 		searchDiv.getElement().getStyle().setVisibility(visibility);
 	}
 }
