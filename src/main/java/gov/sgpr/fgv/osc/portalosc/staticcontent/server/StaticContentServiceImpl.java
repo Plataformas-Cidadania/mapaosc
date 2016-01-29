51d05d5523375e0394c5782e95d07888877696c5
gabrielsqsf <gabrielsqsf@gmail.com>
2016-01-29 11:56:09 -0200

    Issue: MOSC-404 Subject: Implementação da alternativa Iframe e Joomla Detail: início das modificações de XML.

--- src/main/java/gov/sgpr/fgv/osc/portalosc/staticcontent/server/StaticContentServiceImpl.java
+++ src/main/java/gov/sgpr/fgv/osc/portalosc/staticcontent/server/StaticContentServiceImpl.java
@@ -1,9 +1,14 @@
 package gov.sgpr.fgv.osc.portalosc.staticcontent.server;
 
+import java.io.IOException;
 import java.util.HashMap;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
+import javax.xml.parsers.*;
 
 import gov.sgpr.fgv.osc.portalosc.staticcontent.shared.interfaces.StaticContentService;
 import gov.sgpr.fgv.osc.portalosc.staticcontent.shared.model.Content;
@@ -24,6 +29,20 @@ public class StaticContentServiceImpl extends RemoteServiceImpl implements Stati
 		super.init(config);
 		ServletContext context = getServletContext();
 		staticUrl = context.getInitParameter("STATIC_URL");
+		String configurationFile = context.getInitParameter("STATIC_CONFIG"); 
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document document = builder.parse(configurationFile);
+			Element root = document.getDocumentElement();
+			Array<Element> nodes = root.getChildNodes();
+			for(Element element in )
+			{
+				
+			}
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
