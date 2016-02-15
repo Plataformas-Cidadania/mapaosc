00beaaa2cf8a2dfabe7b91a829a23713e0123739
tmnascimento <tmnascimento@gmail.com>
2016-02-15 19:14:45 -0200

    Issue: MOSC-425
    Subject: Atualização do popup da OSC
    Detail:  Atualizar o popup da OSC no mapa conforme protótipo no index.html.

--- src/main/java/gov/sgpr/fgv/osc/portalosc/map/client/components/model/PaperClipCheckListSection.java
+++ src/main/java/gov/sgpr/fgv/osc/portalosc/map/client/components/model/PaperClipCheckListSection.java
@@ -3,16 +3,19 @@ package gov.sgpr.fgv.osc.portalosc.map.client.components.model;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
 
 /**
  * @author Thassae Santos
  * 
  */
 public class PaperClipCheckListSection extends
-		PaperClipAbstractSection<Map<String, Boolean>> {
+		//PaperClipAbstractSection<Map<String, Boolean>> {
+		PaperClipAbstractSection<Map<String, String>> {
 
 	private String sectionTitle;
 	private Map<String, Boolean> checkList = new LinkedHashMap<String, Boolean>();
+	private LinkedHashMap<String, String> keyValueContent = new LinkedHashMap<String, String>();
 
 	/**
 	 * Adiciona um elemento String à seção.
@@ -35,6 +38,30 @@ public class PaperClipCheckListSection extends
 			this.checkList.remove(element);
 		}
 	}
+	
+	/**
+	 * Adiciona um par chave/valor à seção.
+	 * 
+	 * @param key
+	 *            A String que servirá como chave.
+	 * @param value
+	 *            A String que servirá como valor.
+	 */
+	public void addKeyValue(String key, String value) {
+		this.keyValueContent.put(key, value);
+	}
+
+	/**
+	 * Remove um par chave/valor da seção
+	 * 
+	 * @param key
+	 *            A String chave.
+	 */
+	public void removeKeyValue(String key) {
+		if (this.keyValueContent.containsKey(key)) {
+			this.keyValueContent.remove(key);
+		}
+	}
 
 	/*
 	 * (non-Javadoc)
@@ -44,8 +71,11 @@ public class PaperClipCheckListSection extends
 	 * getSectionContent()
 	 */
 	@Override
-	public Map<String, Boolean> getSectionContent() {
+	/*public Map<String, Boolean> getSectionContent() {
 		return checkList;
+	}*/
+	public Map<String, String> getSectionContent() {
+		return keyValueContent;
 	}
 
 	/*
