297675ae7d4316251a436f1b3a54c4fe1fe63648
vagnerpraia <vagnerpraia@gmail.com>
2016-03-21 16:44:51 -0300

    Issue: MOSC-307 Subject: Implementação do serviço de upload dos dados de convênios de Estados e Municípios Detail:

--- src/main/java/gov/sgpr/fgv/osc/portalosc/uploadlocality/server/UploadLocalityServiceImpl.java
+++ src/main/java/gov/sgpr/fgv/osc/portalosc/uploadlocality/server/UploadLocalityServiceImpl.java
@@ -8,7 +8,6 @@ import gov.sgpr.fgv.osc.portalosc.user.shared.exception.RemoteException;
 public class UploadLocalityServiceImpl extends RemoteServiceImpl implements UploadLocalityService {
 	private static final long serialVersionUID = 6487133703022070014L;
 
-	@Override
 	public void uploadFile(AgreementLocalityModel agreement) throws RemoteException {
 		
 	}
