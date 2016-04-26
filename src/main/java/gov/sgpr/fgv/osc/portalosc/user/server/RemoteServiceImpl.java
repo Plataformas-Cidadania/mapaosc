e4d95fe23cd7c1b8ad8c6ac0a01c98d8473442c8
EricFerreiraS <eric@wiiglo.com>
2016-04-26 17:16:47 -0300

    Issue: [MOSC #1.7.1] Subject:Correcao do bug no web-sample.xml Detail:

--- src/main/java/gov/sgpr/fgv/osc/portalosc/user/server/RemoteServiceImpl.java
+++ src/main/java/gov/sgpr/fgv/osc/portalosc/user/server/RemoteServiceImpl.java
@@ -3,11 +3,6 @@
  */
 package gov.sgpr.fgv.osc.portalosc.user.server;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.channels.FileChannel;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,20 +13,8 @@ import java.util.logging.Logger;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.apache.commons.dbcp.BasicDataSource;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
@@ -94,12 +77,12 @@ public abstract class RemoteServiceImpl extends RemoteServiceServlet {
 			} catch (SQLException e) {
 				String message = "Could not find our DataSource in DBManager. We're about to have problems.";
 				logger.log(Level.SEVERE, message);
-				logger.log(Level.SEVERE, "Class: " + this.getClass().getName() + " / Method: init(ServletConfig config)");
+				logger.log(Level.SEVERE,
+						"Class: " + this.getClass().getName() + " / Method: init(ServletConfig config)");
 				logger.log(Level.SEVERE, e.getMessage());
 				e.printStackTrace();
 
 			}
-			copyFile("src/main/webapp/WEB-INF/web.xml", "src/main/webapp/WEB-INF/web-sample.xml");
 		} catch (Exception e) {
 			logger.log(Level.SEVERE, "Class: " + this.getClass().getName() + " / Method: init(ServletConfig config)");
 			logger.log(Level.SEVERE, e.getMessage());
@@ -122,7 +105,8 @@ public abstract class RemoteServiceImpl extends RemoteServiceServlet {
 		try {
 			conn.close();
 		} catch (SQLException e) {
-			logger.log(Level.SEVERE, "Class: " + this.getClass().getName() + " / Method: releaseConnection(Connection conn)");
+			logger.log(Level.SEVERE,
+					"Class: " + this.getClass().getName() + " / Method: releaseConnection(Connection conn)");
 			logger.log(Level.SEVERE, e.getMessage());
 			e.printStackTrace();
 		}
@@ -140,7 +124,8 @@ public abstract class RemoteServiceImpl extends RemoteServiceServlet {
 			}
 			conn.close();
 		} catch (SQLException e) {
-			logger.log(Level.SEVERE, "Class: " + this.getClass().getName() + " / Method: releaseConnection(Connection conn, Statement stmt, ResultSet rs)");
+			logger.log(Level.SEVERE, "Class: " + this.getClass().getName()
+					+ " / Method: releaseConnection(Connection conn, Statement stmt, ResultSet rs)");
 			logger.log(Level.SEVERE, e.getMessage());
 			e.printStackTrace();
 		}
@@ -152,7 +137,8 @@ public abstract class RemoteServiceImpl extends RemoteServiceServlet {
 			stmt = null;
 			conn.close();
 		} catch (SQLException e) {
-			logger.log(Level.SEVERE, "Class: " + this.getClass().getName() + " / Method: releaseConnection(Connection conn, Statement stmt)");
+			logger.log(Level.SEVERE, "Class: " + this.getClass().getName()
+					+ " / Method: releaseConnection(Connection conn, Statement stmt)");
 			logger.log(Level.SEVERE, e.getMessage());
 			e.printStackTrace();
 		}
@@ -170,7 +156,8 @@ public abstract class RemoteServiceImpl extends RemoteServiceServlet {
 			}
 			conn.close();
 		} catch (SQLException e) {
-			logger.log(Level.SEVERE, "Class: " + this.getClass().getName() + " / Method: releaseConnection(Connection conn, PreparedStatement pstmt, ResultSet rs)");
+			logger.log(Level.SEVERE, "Class: " + this.getClass().getName()
+					+ " / Method: releaseConnection(Connection conn, PreparedStatement pstmt, ResultSet rs)");
 			logger.log(Level.SEVERE, e.getMessage());
 			e.printStackTrace();
 		}
@@ -184,7 +171,8 @@ public abstract class RemoteServiceImpl extends RemoteServiceServlet {
 			}
 			conn.close();
 		} catch (SQLException e) {
-			logger.log(Level.SEVERE, "Class: " + this.getClass().getName() + " / Method: releaseConnection(Connection conn, PreparedStatement pstmt) ");
+			logger.log(Level.SEVERE, "Class: " + this.getClass().getName()
+					+ " / Method: releaseConnection(Connection conn, PreparedStatement pstmt) ");
 			logger.log(Level.SEVERE, e.getMessage());
 			e.printStackTrace();
 		}
@@ -202,7 +190,8 @@ public abstract class RemoteServiceImpl extends RemoteServiceServlet {
 			}
 			conn.close();
 		} catch (SQLException e) {
-			logger.log(Level.SEVERE, "Class: " + this.getClass().getName() + " / Method: releaseConnection(Connection conn, PreparedStatement pstmt, PreparedStatement pstmt2");
+			logger.log(Level.SEVERE, "Class: " + this.getClass().getName()
+					+ " / Method: releaseConnection(Connection conn, PreparedStatement pstmt, PreparedStatement pstmt2");
 			logger.log(Level.SEVERE, e.getMessage());
 			e.printStackTrace();
 		}
@@ -213,99 +202,6 @@ public abstract class RemoteServiceImpl extends RemoteServiceServlet {
 	 */
 	public boolean isTestMode() {
 		return testMode;
-	}
-
-	/**
-	 * @return web-sample.xml
-	 */
-	@SuppressWarnings("resource")
-	private void copyFile(final String source1, final String destination1)
-			throws IOException, ParserConfigurationException, SAXException {
-		File source = new File(source1);
-		File destination = new File(destination1);
-		if (destination.exists())
-			destination.delete();
-		FileChannel sourceChannel = null;
-		FileChannel destinationChannel = null;
-		try {
-			sourceChannel = new FileInputStream(source).getChannel();
-			destinationChannel = new FileOutputStream(destination).getChannel();
-			sourceChannel.transferTo(0, sourceChannel.size(), destinationChannel);
-		} finally {
-			if (sourceChannel != null && sourceChannel.isOpen())
-				sourceChannel.close();
-			if (destinationChannel != null && destinationChannel.isOpen())
-				destinationChannel.close();
-		}
-		alterFile();
-	}
-
-	/**
-	 * @return web-sample.xml without some configurations
-	 */
-
-	private void alterFile() throws ParserConfigurationException, SAXException, IOException {
-
-		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-		Document doc = docBuilder.parse(new File("src/main/webapp/WEB-INF/web-sample.xml"));
-
-		try {
-			Element root = doc.getDocumentElement();
-
-			// Return the NodeList on a given tag name
-			NodeList childNodes = root.getElementsByTagName("context-param");
-
-			for (int index = 0; index < childNodes.getLength(); index++) {
-				NodeList subChildNodes = childNodes.item(index).getChildNodes();
-				// logger.info("Item 1 - " +
-				// subChildNodes.item(1).getTextContent());
-				// logger.info("Item 3 - " +
-				// subChildNodes.item(3).getTextContent());
-
-				if (subChildNodes.item(1).getTextContent().equals("ServerName"))
-					subChildNodes.item(3).setTextContent(" ");
-				else if (subChildNodes.item(1).getTextContent().equals("Port"))
-					subChildNodes.item(3).setTextContent(" ");
-				else if (subChildNodes.item(1).getTextContent().equals("DatabaseName"))
-					subChildNodes.item(3).setTextContent(" ");
-				else if (subChildNodes.item(1).getTextContent().equals("UserName"))
-					subChildNodes.item(3).setTextContent(" ");
-				else if (subChildNodes.item(1).getTextContent().equals("Password"))
-					subChildNodes.item(3).setTextContent(" ");
-				else if (subChildNodes.item(1).getTextContent().equals("FACEBOOK_APP_ID"))
-					subChildNodes.item(3).setTextContent(" ");
-				else if (subChildNodes.item(1).getTextContent().equals("FACEBOOK_APP_SECRET"))
-					subChildNodes.item(3).setTextContent(" ");
-				else if (subChildNodes.item(1).getTextContent().equals("FACEBOOK_APP_TOKEN"))
-					subChildNodes.item(3).setTextContent(" ");
-				else if (subChildNodes.item(1).getTextContent().equals("CHAVE_CRIPTOGRAFIA"))
-					subChildNodes.item(3).setTextContent(" ");
-				else if (subChildNodes.item(1).getTextContent().equals("SERVER_SMTP"))
-					subChildNodes.item(3).setTextContent(" ");
-				else if (subChildNodes.item(1).getTextContent().equals("SMTP_AUTH"))
-					subChildNodes.item(3).setTextContent(" ");
-				else if (subChildNodes.item(1).getTextContent().equals("SMTP_PORT"))
-					subChildNodes.item(3).setTextContent(" ");
-				else if (subChildNodes.item(1).getTextContent().equals("FROM_ADDRESS"))
-					subChildNodes.item(3).setTextContent(" ");
-				else if (subChildNodes.item(1).getTextContent().equals("NAME_ADDRESS"))
-					subChildNodes.item(3).setTextContent(" ");
-
-			}
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
-			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(new File("src/main/webapp/WEB-INF/web-sample.xml"));
-			transformer.transform(source, result);
-
-			logger.info("web-sample.xml was created.");
-
-		} catch (TransformerException e) {
-			logger.log(Level.SEVERE, "Class: " + this.getClass().getName() + " / Method: alterFile()");
-			logger.log(Level.SEVERE, e.getMessage());
-			e.printStackTrace();
-		}
 	}
 
 }
