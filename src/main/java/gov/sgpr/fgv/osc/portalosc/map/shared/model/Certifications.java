8a0ecd3bddcecfa2ac925fe2b588001c20871b3a
tmnascimento <tmnascimento@gmail.com>
2016-07-28 17:49:10 -0300

    Update Certifications.java

--- src/main/java/gov/sgpr/fgv/osc/portalosc/map/shared/model/Certifications.java
+++ src/main/java/gov/sgpr/fgv/osc/portalosc/map/shared/model/Certifications.java
@@ -285,16 +285,18 @@ public class Certifications implements Serializable,
 
 		key = "OSCIP";
 		begin = this.oscipPublication;
-		if (begin != null){
+		if (begin != null) {
 			if (Dtfmt.format(begin) == "01/01/1900") {
 			value = "Validade não informada";
 			}
 			else {
  			value = "Válido desde " + Dtfmt.format(begin);
 			}
- 		} else {
+ 		} 
+ 		else {
 			value = "Não possui";
 		}
+		
 		content.put(key, value);
 
 		key = "UPF";
